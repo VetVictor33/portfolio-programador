a007a4bca598702ec57fb8b807f380341ae45cb6
Victor Feliciano <victorjfeliciano@gmail.com>
2023-02-12 19:47:58 -0300

    usando javascript pra adicionar cursos complementares de maneira dinamica

---
+++ pages/about/script.js
@@ -0,0 +1,50 @@
+const compEdu = document.querySelector('.complementary');
+
+function postCompEdu() {
+    const comEduArray = [
+        {
+            title: "Python para Data Science",
+            date: "2021",
+            description: "Princípios de Dada Science e Python com uso de Anaconda e Júpiter. Introdução à análise de dados"
+        },
+        {
+            title: "Programação em Android básica pts1, 2 e 3",
+            date: "2021",
+            description: "Princípios da programação mobile com uso de Java e Android Studio"
+        },
+        {
+            title: "Java e Orientação a Objetos",
+            date: "2021",
+            description: "Princípios da orientação a objetos e bases de Java com uso de Eclipse IDE"
+        },
+        {
+            title: "Iniciante em Programação(Alura)",
+            date: "2021",
+            description: "Princípios da lógica de programação, introdução a HTML, CSS e JavaScript"
+        }
+    ]
+
+    for (let i = 0; i < comEduArray.length; i++) {
+        const newCourse = document.createElement('article');
+        const newTitle = document.createElement('p');
+        const newDate = document.createElement('p');
+        const newDescription = document.createElement('p');
+
+        compEdu.appendChild(newCourse);
+        newCourse.appendChild(newTitle);
+        newCourse.appendChild(newDate);
+        newCourse.appendChild(newDescription);
+
+        newCourse.classList.add('course');
+        newTitle.classList.add('text-description', 'bigger');
+        newDate.classList.add('text-description', 'smaller');
+        newDescription.classList.add('text-description', 'medium');
+
+        newTitle.innerHTML = comEduArray[i].title;
+        newDate.innerHTML = comEduArray[i].date;
+        newDescription.innerHTML = comEduArray[i].description;
+
+    }
+}
+
+postCompEdu();
