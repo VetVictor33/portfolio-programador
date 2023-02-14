ce44739e4f91129a5a98b43f1798bcc47819420b
Victor Feliciano <victorjfeliciano@gmail.com>
2023-02-14 14:49:20 -0300

    corrigindo bugs da img src da homepage

--- script/script.js
+++ script/script.js
@@ -10,43 +10,43 @@ function link(card, adress) {
 function postProjects() {
     const projectsArrays = [
         {
-            imgSrc: "../assets/projects/curriculo-web.png",
+            imgSrc: "./assets/projects/curriculo-web.png",
             title: "Currículo Web",
             description: "Primeiro projeto realizado para sedimentar os conhecimentos em HTML5 e CSS3, além de arriscar um pouco de JavaScript",
             link: "https://github.com/VetVictor33/curriculo-web"
         },
         {
-            imgSrc: "../assets/projects/portfolio-escritor.png",
+            imgSrc: "./assets/projects/portfolio-escritor.png",
             title: "Portfoílio de Escritor",
             description: "Utilizei HTML5, CSS3 e um pouco de JavaScript para apresentar alguns dos textos que produzi para um projeto pessoal relacionado à produção de crônicas e contos.",
             link: "https://github.com/VetVictor33/portfolio-escritor"
         },
         {
-            imgSrc: "../assets/projects/duolingo-clone.png",
+            imgSrc: "./assets/projects/duolingo-clone.png",
             title: "Clone da home page do Duolingo",
             description: "Uma forma que encontrei de treinar como seria atender a uma demanda de um designer ao reproduzir a página do site de ensino de idiomas. Ainda não adicionei responsividade, porém planejo fazê-lo no futuro.",
             link: "https://github.com/VetVictor33/clone-duolingo"
         },
         {
-            imgSrc: "../assets/projects/desafio-portfolio.png",
+            imgSrc: "./assets/projects/desafio-portfolio.png",
             title: "Portfólio",
             description: "Nesse projeto criei um portfólio fictício com base em um layout do figma como forma de avaliação de um curso. As tecnologias utilizadas foram CSS e HTML.",
             link: "https://github.com/VetVictor33/desafio-portfolio"
         },
         {
-            imgSrc: "../assets/projects/form-signup.png",
+            imgSrc: "./assets/projects/form-signup.png",
             title: "Formulário de inscrição",
             description: "Projeto passado como exercício de um curso no qual foi proposto o desenvolvimento de um formulário de inscrição em vestibular. Foi utilizado HTML, CSS e JavaScript para validar os dados.",
             link: "https://github.com/VetVictor33/formulario-inscricao"
         },
         {
-            imgSrc: "../assets/projects/form.png",
+            imgSrc: "./assets/projects/form.png",
             title: "Formulário para Login",
             description: "Projeto passado como exercício de um curso no qual foi proposto o desenvolvimento do Front End de login e sign in de um site fictício com base em um layout do figma. Foi utilizado HTML, CSS e JavaScript para validar os dados e adicionar comportamento ao site.",
             link: "https://github.com/VetVictor33/desafio-02"
         },
         {
-            imgSrc: "../assets/projects/tech-store.png",
+            imgSrc: "./assets/projects/tech-store.png",
             title: "Tech Store",
             description: "Loja de produtos eletrônicos feita baseada em um curso da Skillshare. As technologias utilizadas foram HTML, CSS e JavaScript. Lancei mão da criação de novos elementos da DOM por meio do script para dar comportamento ao site, permitindo selecionar itens específicos, relizar filtragem por preço, categoria, além de busca pelo nome do produto, tudo isso sem necessidade de trocar a página. Também brinquei um pouco com Local Storage de forma a salvar os item selecionados pelo usuário e colocá-los no carrinho de compras.",
             link: "https://github.com/VetVictor33/tech-store-skillshare"
