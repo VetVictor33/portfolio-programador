6eb77b04b62cc86bd0a206d53e6d1299047ff6d6
Victor Feliciano <victorjfeliciano@gmail.com>
2023-03-27 11:17:55 -0300

    add modal

---
+++ script/modal.js
@@ -0,0 +1,6 @@
+const modal = document.querySelector('dialog');
+const modalLink = document.querySelector('.modal a');
+
+modal.showModal();
+
+modalLink.setAttribute('href', "https://victor-feliciano.onrender.com/");
